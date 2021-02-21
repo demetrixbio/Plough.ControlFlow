f3cc01b7ba567d0b5f761a25f11ccd7241b541f4
Gien Verschatse <gien@eightpointsquared.com>
2021-02-21 16:46:46 +0100

    Set up publish to nuget

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,2 @@
-### 0.1.0
-
+## New in 0.1.0 (Released 2021/02/21)
 * Release initial ControlFlow library of Plough
--- build.fsx
+++ build.fsx
@@ -1,7 +1,9 @@
 #r "paket:
   nuget Fake.DotNet.Cli
   nuget Fake.IO.FileSystem
-  nuget Fake.Core.Target //"
+  nuget Fake.Core.Target
+  nuget Fake.Core.ReleaseNotes
+  nuget Fake.DotNet.Paket //"
 #load ".fake/build.fsx/intellisense.fsx"
 
 open Fake.Core
@@ -38,8 +40,26 @@ Target.create "Test" (fun _ ->
   |> Seq.iter (DotNet.test id)
 )
 
+// PUBLISH TO NUGET
+Target.create "Pack" (fun _ ->
+    let release = ReleaseNotes.load "RELEASE_NOTES.md"
+
+    Paket.pack(fun p ->
+        { p with
+            BuildConfig = "Release"
+            OutputPath = "bin"
+            MinimumFromLockFile = true
+            IncludeReferencedProjects = true
+            Version = release.AssemblyVersion
+            TemplateFile = "paket.template"
+            ReleaseNotes = String.toLines release.Notes })
+)
+"Build" ==> "Pack"
+
 Target.create "Publish" (fun _ ->
-  
+    Paket.push(fun p ->
+        { p with
+            WorkingDir = "bin" })
 )
 
 Target.create "All" ignore
